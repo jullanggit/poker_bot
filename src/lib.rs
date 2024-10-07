bfd9fd9d07f4a3b3a784ffbd80472d5df63b943d
jullang <jullanggit@proton.me>
2024-10-07 22:51:55 +0200

    5x faster!! changed highest_possible hand to not require internal combinations and instead operate on a single sorted copy of all 7 cards

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(let_chains)]
+
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
@@ -9,13 +11,15 @@ use std::{
     io,
     mem::transmute,
     num::{ParseFloatError, ParseIntError},
+    ops::AddAssign,
+    str::FromStr,
     sync::atomic::{self, AtomicU32},
 };
 use strum_macros::EnumCount;
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[repr(transparent)]
-struct Card {
+pub struct Card {
     // First 4 bits = Value
     // Jack - Ace = 11 - 14
     //
@@ -24,7 +28,39 @@ struct Card {
     // 01 = Diamonds
     // 10 = Clubs
     // 11 = Spades
-    value: u8,
+    _inner: u8,
+}
+impl Card {
+    pub fn new(value: CardValue, color: Color) -> Self {
+        // Safe because of conversion to u8 from valid values
+        unsafe { Self::_from_num_unchecked(value as u8, color as u8) }
+    }
+    pub fn from_num(value: u8, color: u8) -> Self {
+        assert!((2..=14).contains(&value));
+        assert!((0..=3).contains(&color));
+
+        unsafe { Self::_from_num_unchecked(value, color) }
+    }
+    unsafe fn _from_num_unchecked(value: u8, color: u8) -> Self {
+        Self {
+            _inner: value + ((color) << 4),
+        }
+    }
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        let value = rng.gen_range(2..=14);
+        let color = rng.gen_range(0..=3);
+
+        Self::from_num(value, color)
+    }
+    const fn value(&self) -> CardValue {
+        // Safe because of the manually set discriminants
+        unsafe { transmute(self._inner & 0b0000_1111) }
+    }
+    const fn color(&self) -> Color {
+        // Safe because of the manually set discriminants
+        unsafe { transmute(self._inner >> 4) }
+    }
 }
 impl TryFrom<&str> for Card {
     type Error = &'static str;
@@ -35,59 +71,75 @@ impl TryFrom<&str> for Card {
             return Err("Invalid card format");
         }
 
-        let value = match card_parts[0] {
-            "Ace" | "14" | "a" | "A" => Ok(14),
-            "2" => Ok(2),
-            "3" => Ok(3),
-            "4" => Ok(4),
-            "5" => Ok(5),
-            "6" => Ok(6),
-            "7" => Ok(7),
-            "8" => Ok(8),
-            "9" => Ok(9),
-            "10" => Ok(10),
-            "Jack" | "11" | "j" | "J" => Ok(11),
-            "Queen" | "12" | "q" | "Q" => Ok(12),
-            "King" | "13" | "k" | "K" => Ok(13),
-            // maybe add error handling
-            _otherwise => Err("Failed to parse value"),
-        }?;
-        let color = match card_parts[1] {
-            "Hearts" | "1" => Ok(1),
-            "Diamonds" | "2" => Ok(2),
-            "Clubs" | "3" => Ok(3),
-            "Spades" | "4" => Ok(4),
-            // maybe add error handling
-            _otherwise => Err("Failed to parse color"),
-        }?;
+        let value = card_parts[0].parse()?;
+        let color = card_parts[1].parse()?;
 
         Ok(Self::new(value, color))
     }
 }
-impl Card {
-    const fn new(value: u8, color: u8) -> Self {
-        Self {
-            value: value + (color << 4),
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum CardValue {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14,
+}
+impl FromStr for CardValue {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "2" => Ok(Self::Two),
+            "3" => Ok(Self::Three),
+            "4" => Ok(Self::Four),
+            "5" => Ok(Self::Five),
+            "6" => Ok(Self::Six),
+            "7" => Ok(Self::Seven),
+            "8" => Ok(Self::Eight),
+            "9" => Ok(Self::Nine),
+            "10" => Ok(Self::Ten),
+            "Jack" | "11" | "j" | "J" => Ok(Self::Jack),
+            "Queen" | "12" | "q" | "Q" => Ok(Self::Queen),
+            "King" | "13" | "k" | "K" => Ok(Self::King),
+            "Ace" | "1" | "14" | "a" | "A" => Ok(Self::Ace),
+            // maybe add error handling
+            _otherwise => Err("Failed to parse value"),
         }
     }
-    fn random() -> Self {
-        let mut rng = thread_rng();
-        let value = rng.gen_range(2..=14);
-        let color = rng.gen_range(0..=3);
+}
 
-        Self::new(value, color)
-    }
-    const fn value(&self) -> u8 {
-        self.value & 0b0000_1111
-    }
-    const fn color(&self) -> u8 {
-        self.value >> 4
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub enum Color {
+    Hearts = 0,
+    Diamonds = 1,
+    Clubs = 2,
+    Spades = 3,
+}
+impl FromStr for Color {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Hearts" | "1" => Ok(Self::Hearts),
+            "Diamonds" | "2" => Ok(Self::Diamonds),
+            "Clubs" | "3" => Ok(Self::Clubs),
+            "Spades" | "4" => Ok(Self::Spades),
+            // maybe add error handling
+            _otherwise => Err("Failed to parse color"),
+        }
     }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Copy, EnumCount)]
-#[repr(u8)]
-enum Hand {
+pub enum Hand {
     RoyalFlush = 9,
     StraightFlush = 8,
     FourOfAKind = 7,
@@ -98,13 +150,6 @@ enum Hand {
     TwoPair = 2,
     Pair = 1,
     HighCard = 0,
-}
-impl Hand {
-    fn lower(self) -> Self {
-        let lowered_u8 = (self as u8).saturating_sub(1);
-
-        unsafe { transmute(lowered_u8) }
-    }
 }
 
 #[derive(Default, Debug)]
@@ -148,7 +193,10 @@ pub fn calculate(interactive: bool) {
         .par_bridge()
         .for_each(|remaining_pool| {
             let entire_pool = pool.iter().cloned().chain(remaining_pool.clone());
-            let player_combined = entire_pool.clone().chain(player_cards.iter().cloned());
+            let player_combined = entire_pool
+                .clone()
+                .chain(player_cards.iter().cloned())
+                .collect();
             let player_hand = highest_possible_hand(player_combined, None);
             // Calculate win percentage
 
@@ -159,7 +207,7 @@ pub fn calculate(interactive: bool) {
                 // Get possible other hand cards
                 .combinations(2)
                 .for_each(|other_cards| {
-                    let other_combined = entire_pool.clone().chain(other_cards);
+                    let other_combined = entire_pool.clone().chain(other_cards).collect();
                     let other_hand = highest_possible_hand(other_combined, Some(player_hand));
 
                     // Aggregate wins, losses and draws
@@ -255,7 +303,8 @@ fn create_deck() -> Vec<Card> {
     let mut deck = Vec::new();
     for color in 0..=3 {
         for value in 2..=14 {
-            deck.push(Card::new(value, color));
+            // Safe because of above loop bounds
+            deck.push(Card::from_num(value, color));
         }
     }
     deck
@@ -272,123 +321,202 @@ fn best_bet(win_chance: f64, pot: f64, min_bet: u32, max_bet: u32) -> (u32, f64)
         .unwrap()
 }
 
-fn highest_possible_hand<T>(input_cards: T, player_hand: Option<Hand>) -> Hand
-where
-    T: Iterator<Item = Card>,
-{
-    let mut highest_hand = player_hand.map_or(Hand::HighCard, Hand::lower);
-    for mut cards in input_cards.combinations(5) {
-        // Sort cards by their position in SEQUENCE
-        cards.sort_by_key(Card::value);
-
-        let is_straight = is_straight(&cards);
-        let is_flush = is_flush(&cards);
-        let mut is_three_of_a_kind_bool = false;
-        let mut pairs = 0;
-
-        // royal flush
-        if is_straight && is_flush {
-            if cards.iter().any(|card| card.value() == 13)
-                && cards.iter().any(|card| card.value() == 14)
-            {
-                highest_hand = Hand::RoyalFlush;
-                break;
-            }
-            highest_hand = Hand::StraightFlush;
-            continue;
-        } else if highest_hand > Hand::FourOfAKind
-            && cards
-                .iter()
-                .combinations(4)
-                .any(|local_cards| is_four_of_a_kind(&local_cards))
-        {
-            highest_hand = Hand::FourOfAKind;
-            continue;
-        } else if highest_hand > Hand::FullHouse
-            && is_full_house(&cards, &mut is_three_of_a_kind_bool, &mut pairs)
-        {
-            highest_hand = Hand::FullHouse;
-            continue;
-        } else if highest_hand > Hand::Flush && is_flush {
-            highest_hand = Hand::Flush;
-            continue;
-        } else if highest_hand > Hand::Straight && is_straight {
-            highest_hand = Hand::Straight;
-            continue;
-        } else if highest_hand > Hand::ThreeOfAKind && is_three_of_a_kind_bool {
-            highest_hand = Hand::ThreeOfAKind;
-            continue;
-        } else if highest_hand > Hand::TwoPair && pairs >= 2 {
-            highest_hand = Hand::TwoPair;
-            continue;
-        } else if highest_hand > Hand::Pair && pairs >= 1 {
-            highest_hand = Hand::Pair;
+#[derive(Default)]
+struct ColorsCounter {
+    hearts: u8,
+    diamonds: u8,
+    clubs: u8,
+    spades: u8,
+}
+impl ColorsCounter {
+    fn flush(&self) -> Option<Color> {
+        if self.hearts >= 5 {
+            Some(Color::Hearts)
+        } else if self.diamonds >= 5 {
+            Some(Color::Diamonds)
+        } else if self.clubs >= 5 {
+            Some(Color::Clubs)
+        } else if self.spades >= 5 {
+            Some(Color::Spades)
+        } else {
+            None
         }
     }
-    highest_hand
 }
-
-const fn is_four_of_a_kind(cards: &[&Card]) -> bool {
-    cards[0].value() == cards[1].value()
-        && cards[0].value() == cards[2].value()
-        && cards[0].value() == cards[3].value()
-}
-
-// TODO: Make it actually only count if the pair and the three of a kind is seperate
-fn is_full_house(
-    cards: &[Card],
-    is_three_of_a_kind_bool: &mut bool,
-    pairs_usize: &mut usize,
-) -> bool {
-    let mut pair_value = 0;
-    let mut three_of_a_kind_value = 0;
-    *pairs_usize = cards
-        .iter()
-        .combinations(2)
-        .filter(|cards| is_pair(&cards[0..2], &mut pair_value))
-        .count();
-    *is_three_of_a_kind_bool = cards
-        .iter()
-        .combinations(3)
-        .any(|cards| is_three_of_a_kind(&cards[0..3], &mut three_of_a_kind_value));
-    *is_three_of_a_kind_bool && *pairs_usize > 0 && pair_value != three_of_a_kind_value
-}
-
-const fn is_flush(cards: &[Card]) -> bool {
-    cards[0].color() == cards[1].color()
-        && cards[0].color() == cards[2].color()
-        && cards[0].color() == cards[3].color()
-        && cards[0].color() == cards[4].color()
-}
-
-const fn is_straight(cards: &[Card]) -> bool {
-    // normal case
-    (cards[1].value() == cards[0].value() + 1
-        && cards[2].value() == cards[0].value() + 2
-        && cards[3].value() == cards[0].value() + 3
-        && cards[4].value() == cards[0].value() + 4)
-        // edge case Ace,2,3,4,5
-        || (cards[0].value() == 2
-            && cards[1].value() == 3
-            && cards[2].value() == 4
-            && cards[3].value() == 5
-            && cards[4].value() == 14)
-}
-
-fn is_three_of_a_kind(cards: &[&Card], value: &mut u8) -> bool {
-    if cards[0].value() == cards[1].value() && cards[0].value() == cards[2].value() {
-        *value = cards[0].value();
-        true
-    } else {
-        false
+impl AddAssign<Color> for ColorsCounter {
+    fn add_assign(&mut self, rhs: Color) {
+        match rhs {
+            Color::Hearts => self.hearts += 1,
+            Color::Diamonds => self.diamonds += 1,
+            Color::Clubs => self.clubs += 1,
+            Color::Spades => self.spades += 1,
+        }
     }
 }
 
-fn is_pair(cards: &[&Card], value: &mut u8) -> bool {
-    if cards[0].value() == cards[1].value() {
-        *value = cards[0].value();
-        true
-    } else {
-        false
+#[derive(Debug)]
+struct StraightStuff {
+    start: CardValue,
+    end: CardValue,
+    flush_counter: u8,
+    flush_end: Option<CardValue>,
+    unsure: bool,
+}
+impl From<(Card, Option<Color>)> for StraightStuff {
+    fn from((card, flush_color): (Card, Option<Color>)) -> Self {
+        let card_is_flush = card_is_flush(card, flush_color);
+
+        Self {
+            start: card.value(),
+            end: card.value(),
+            flush_counter: if card_is_flush { 1 } else { 0 },
+            flush_end: if card_is_flush {
+                Some(card.value())
+            } else {
+                None
+            },
+            unsure: false,
+        }
     }
+}
+impl StraightStuff {
+    fn is_straight(&self) -> bool {
+        self.end as u8 - self.start as u8 >= 4
+    }
+    fn is_flush(&self) -> bool {
+        self.flush_counter >= 5
+    }
+}
+fn card_is_flush(card: Card, flush_color: Option<Color>) -> bool {
+    match flush_color {
+        Some(flush_color) => card.color() == flush_color,
+        _ => false,
+    }
+}
+
+/// Returns HighCard if no hand >= player_hand is found
+pub fn highest_possible_hand(mut input_cards: Vec<Card>, player_hand: Option<Hand>) -> Hand {
+    let highest_hand = player_hand.unwrap_or(Hand::HighCard);
+
+    input_cards.sort_by_key(Card::value);
+
+    let flush = input_cards
+        .iter()
+        .fold(ColorsCounter::default(), |mut counter, card| {
+            counter += card.color();
+            counter
+        })
+        .flush();
+
+    let mut straight_stuff: StraightStuff = (input_cards[0], flush).into();
+    // TODO: See if into_iter is faster
+    for cur_card in input_cards.iter().skip(1) {
+        let diff = cur_card.value() as u8 - straight_stuff.end as u8;
+        if diff == 0 {
+            if straight_stuff.unsure && card_is_flush(*cur_card, flush) {
+                straight_stuff.unsure = false;
+            }
+        // If the current card is consecutive to the end of the current straight
+        } else if diff == 1 {
+            straight_stuff.end = cur_card.value();
+
+            if straight_stuff.unsure {
+                straight_stuff.flush_counter = 0;
+                straight_stuff.flush_end = None;
+            }
+
+            if card_is_flush(*cur_card, flush) {
+                straight_stuff.flush_counter += 1;
+                straight_stuff.flush_end = Some(cur_card.value());
+            // Set unsure because maybe theres another card of the same value but with the right suit
+            // Dont set unsure if there already is a straight flush
+            } else if straight_stuff.flush_counter < 5 {
+                straight_stuff.unsure = true
+            }
+        } else if diff > 1 {
+            if straight_stuff.is_straight() {
+                break;
+            }
+            straight_stuff = (*cur_card, flush).into()
+        }
+    }
+    let is_straight = straight_stuff.is_straight();
+
+    let is_straight_flush = is_straight && straight_stuff.is_flush();
+
+    // Royal flush
+    if is_straight_flush && straight_stuff.flush_end == Some(CardValue::Ace) {
+        return Hand::RoyalFlush;
+    }
+
+    if is_straight_flush {
+        return Hand::StraightFlush;
+    }
+
+    if highest_hand > Hand::FourOfAKind {
+        return Hand::HighCard;
+    }
+    // StraightFlush
+    // Four of a kind, Full house, Three of a kind & pairs
+    let mut pairs = 0;
+    let mut is_full_house = false;
+    let mut is_three_of_a_kind = false;
+    for base_index in 0..=5 {
+        let (a, b, c, d, e) = (
+            input_cards.get(base_index),
+            input_cards.get(base_index + 1),
+            input_cards.get(base_index + 2),
+            input_cards.get(base_index + 3),
+            input_cards.get(base_index + 4),
+        );
+        match (a, b, c, d, e) {
+            (Some(a), Some(b), Some(c), Some(d), _)
+                if a.value() == b.value() && b.value() == c.value() && c.value() == d.value() =>
+            {
+                // Return because this is the highest achievable hand at this point
+                return Hand::FourOfAKind;
+            }
+            // TODO: See if short-circuiting here is faster
+            (Some(a), Some(b), Some(c), Some(d), Some(e))
+                if a.value() == b.value() && b.value() == c.value() && d.value() == e.value()
+                    || a.value() == b.value()
+                        && c.value() == d.value()
+                        && d.value() == e.value() =>
+            {
+                // Cant return because it can still be a four of a kind
+                is_full_house = true
+            }
+            // TODO: See if short-circuiting here is faster (straight & flush and full_house)
+            (Some(a), Some(b), Some(c), _, _)
+                if !is_straight
+                    && flush.is_none()
+                    && a.value() == b.value()
+                    && b.value() == c.value() =>
+            {
+                // Cant return because it can still be a four of a kind
+                is_three_of_a_kind = true
+            }
+            (Some(a), Some(b), _, _, _) if a.value() == b.value() => pairs += 1,
+            _ => {}
+        }
+    }
+    if is_full_house {
+        return Hand::FullHouse;
+    }
+    if flush.is_some() {
+        return Hand::Flush;
+    }
+    if is_straight {
+        return Hand::Straight;
+    }
+    if is_three_of_a_kind {
+        return Hand::ThreeOfAKind;
+    }
+    if pairs >= 2 {
+        return Hand::TwoPair;
+    }
+    if pairs == 1 {
+        return Hand::Pair;
+    }
+    Hand::HighCard
 }
