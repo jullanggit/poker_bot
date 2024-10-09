c50194f777e5968f182de42c2a8da4d10f233155
jullang <jullanggit@proton.me>
2024-10-09 16:59:11 +0200

    moved io stuff into io.rs

---
+++ src/io.rs
@@ -0,0 +1,129 @@
+use crate::{Card, CardValue, Color};
+use rand::{thread_rng, Rng};
+use std::{
+    io,
+    num::{ParseFloatError, ParseIntError},
+    str::FromStr,
+};
+
+impl FromStr for Card {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Split the string into Color and Value
+        let card_parts: Vec<&str> = s.split(',').collect();
+        if card_parts.len() != 2 {
+            return Err("Invalid card format");
+        }
+
+        let value = card_parts[0].parse()?;
+        let color = card_parts[1].parse()?;
+
+        Ok(Self::new(value, color))
+    }
+}
+
+impl FromStr for CardValue {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "2" => Ok(Self::Two),
+            "3" => Ok(Self::Three),
+            "4" => Ok(Self::Four),
+            "5" => Ok(Self::Five),
+            "6" => Ok(Self::Six),
+            "7" => Ok(Self::Seven),
+            "8" => Ok(Self::Eight),
+            "9" => Ok(Self::Nine),
+            "10" => Ok(Self::Ten),
+            "Jack" | "11" | "j" | "J" => Ok(Self::Jack),
+            "Queen" | "12" | "q" | "Q" => Ok(Self::Queen),
+            "King" | "13" | "k" | "K" => Ok(Self::King),
+            "Ace" | "1" | "14" | "a" | "A" => Ok(Self::Ace),
+            // maybe add error handling
+            _otherwise => Err("Failed to parse value"),
+        }
+    }
+}
+
+impl FromStr for Color {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Hearts" | "1" => Ok(Self::Hearts),
+            "Diamonds" | "2" => Ok(Self::Diamonds),
+            "Clubs" | "3" => Ok(Self::Clubs),
+            "Spades" | "4" => Ok(Self::Spades),
+            // maybe add error handling
+            _otherwise => Err("Failed to parse color"),
+        }
+    }
+}
+
+pub fn get_cards(interactive: bool) -> Result<Vec<Card>, &'static str> {
+    if interactive {
+        // get input for cards
+        println!("Please input your handcards and the cards in the middle in the following format, separated by whitespace: Value,Color");
+        let mut hand_buffer = String::new();
+        let _ = io::stdin().read_line(&mut hand_buffer);
+
+        // Seperate the Cards
+        let cards_string: Vec<&str> = hand_buffer.split_whitespace().collect();
+
+        if cards_string.len() > 7 {
+            return Err("Please input at max 7 cards");
+        }
+
+        cards_string.iter().map(|&string| string.parse()).collect()
+    } else {
+        // let num = thread_rng().gen_range(4..=7);
+        let mut cards = Vec::new();
+        for _ in 0..5 {
+            cards.push(Card::random());
+        }
+        Ok(cards)
+    }
+}
+
+pub fn get_player_count(interactive: bool) -> Result<i32, ParseIntError> {
+    if interactive {
+        println!("Please input the amount of other players participating");
+        let mut player_count_buffer = String::new();
+        let _ = io::stdin().read_line(&mut player_count_buffer);
+
+        player_count_buffer.trim_end().parse::<i32>()
+    } else {
+        Ok(thread_rng().gen_range(1..7))
+    }
+}
+
+pub fn get_pot(interactive: bool) -> Result<f64, ParseFloatError> {
+    if interactive {
+        println!("Please input how much is currently in the pot");
+        let mut input_buffer = String::new();
+        let _ = io::stdin().read_line(&mut input_buffer);
+
+        input_buffer.trim_end().parse::<f64>()
+    } else {
+        Ok(thread_rng().gen_range(10.0..100_000.0))
+    }
+}
+
+pub fn get_min_max_bet(interactive: bool) -> Result<(u32, u32), ParseIntError> {
+    if interactive {
+        println!("Please input the minimum and maximum bet, seperated by spaces");
+
+        let mut input_buffer = String::new();
+        let _ = io::stdin().read_line(&mut input_buffer);
+
+        let mut min_max = input_buffer.split_whitespace();
+
+        let min = min_max.next().unwrap().parse()?;
+        let max = min_max.next().unwrap().parse()?;
+
+        Ok((min, max))
+    } else {
+        let min = thread_rng().gen_range(1..1000);
+        let max = min + thread_rng().gen_range(1..5000);
+        Ok((min, max))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,19 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+use io::{get_cards, get_min_max_bet, get_player_count, get_pot};
 use itertools::Itertools;
-
 use rand::{thread_rng, Rng};
 use rayon::iter::{ParallelBridge, ParallelIterator};
 use std::{
     fmt::Display,
-    io,
     mem::transmute,
-    num::{ParseFloatError, ParseIntError},
     ops::AddAssign,
-    str::FromStr,
     sync::atomic::{self, AtomicU32},
 };
 use strum_macros::EnumCount;
+
+mod io;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[repr(transparent)]
@@ -68,21 +67,6 @@ impl Card {
         unsafe { transmute(self.inner >> 4) }
     }
 }
-impl TryFrom<&str> for Card {
-    type Error = &'static str;
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        // Split the string into Color and Value
-        let card_parts: Vec<&str> = s.split(',').collect();
-        if card_parts.len() != 2 {
-            return Err("Invalid card format");
-        }
-
-        let value = card_parts[0].parse()?;
-        let color = card_parts[1].parse()?;
-
-        Ok(Self::new(value, color))
-    }
-}
 impl Display for Card {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Color: {:?}, Value: {:?}", self.color(), self.value())
@@ -105,28 +89,6 @@ pub enum CardValue {
     King = 13,
     Ace = 14,
 }
-impl FromStr for CardValue {
-    type Err = &'static str;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "2" => Ok(Self::Two),
-            "3" => Ok(Self::Three),
-            "4" => Ok(Self::Four),
-            "5" => Ok(Self::Five),
-            "6" => Ok(Self::Six),
-            "7" => Ok(Self::Seven),
-            "8" => Ok(Self::Eight),
-            "9" => Ok(Self::Nine),
-            "10" => Ok(Self::Ten),
-            "Jack" | "11" | "j" | "J" => Ok(Self::Jack),
-            "Queen" | "12" | "q" | "Q" => Ok(Self::Queen),
-            "King" | "13" | "k" | "K" => Ok(Self::King),
-            "Ace" | "1" | "14" | "a" | "A" => Ok(Self::Ace),
-            // maybe add error handling
-            _otherwise => Err("Failed to parse value"),
-        }
-    }
-}
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum Color {
@@ -134,19 +96,6 @@ pub enum Color {
     Diamonds = 1,
     Clubs = 2,
     Spades = 3,
-}
-impl FromStr for Color {
-    type Err = &'static str;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Hearts" | "1" => Ok(Self::Hearts),
-            "Diamonds" | "2" => Ok(Self::Diamonds),
-            "Clubs" | "3" => Ok(Self::Clubs),
-            "Spades" | "4" => Ok(Self::Spades),
-            // maybe add error handling
-            _otherwise => Err("Failed to parse color"),
-        }
-    }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Copy, EnumCount)]
@@ -235,78 +184,6 @@ pub fn calculate(interactive: bool) {
         println!("Win chance: {}%", win_chance * 100.);
         let (best_bet, ev) = best_bet(win_chance, pot, min_bet, max_bet);
         println!("Best bet: {best_bet}, ev: {ev}",);
-    }
-}
-
-fn get_cards(interactive: bool) -> Result<Vec<Card>, &'static str> {
-    if interactive {
-        // get input for cards
-        println!("Please input your handcards and the cards in the middle in the following format, separated by whitespace: Value,Color");
-        let mut hand_buffer = String::new();
-        let _ = io::stdin().read_line(&mut hand_buffer);
-
-        // Seperate the Cards
-        let cards_string: Vec<&str> = hand_buffer.split_whitespace().collect();
-
-        if cards_string.len() > 7 {
-            return Err("Please input at max 7 cards");
-        }
-
-        cards_string
-            .iter()
-            .map(|&string| string.try_into())
-            .collect()
-    } else {
-        // let num = thread_rng().gen_range(4..=7);
-        let mut cards = Vec::new();
-        for _ in 0..5 {
-            cards.push(Card::random());
-        }
-        Ok(cards)
-    }
-}
-
-fn get_player_count(interactive: bool) -> Result<i32, ParseIntError> {
-    if interactive {
-        println!("Please input the amount of other players participating");
-        let mut player_count_buffer = String::new();
-        let _ = io::stdin().read_line(&mut player_count_buffer);
-
-        player_count_buffer.trim_end().parse::<i32>()
-    } else {
-        Ok(thread_rng().gen_range(1..7))
-    }
-}
-
-fn get_pot(interactive: bool) -> Result<f64, ParseFloatError> {
-    if interactive {
-        println!("Please input how much is currently in the pot");
-        let mut input_buffer = String::new();
-        let _ = io::stdin().read_line(&mut input_buffer);
-
-        input_buffer.trim_end().parse::<f64>()
-    } else {
-        Ok(thread_rng().gen_range(10.0..100_000.0))
-    }
-}
-
-fn get_min_max_bet(interactive: bool) -> Result<(u32, u32), ParseIntError> {
-    if interactive {
-        println!("Please input the minimum and maximum bet, seperated by spaces");
-
-        let mut input_buffer = String::new();
-        let _ = io::stdin().read_line(&mut input_buffer);
-
-        let mut min_max = input_buffer.split_whitespace();
-
-        let min = min_max.next().unwrap().parse()?;
-        let max = min_max.next().unwrap().parse()?;
-
-        Ok((min, max))
-    } else {
-        let min = thread_rng().gen_range(1..1000);
-        let max = min + thread_rng().gen_range(1..5000);
-        Ok((min, max))
     }
 }
 
@@ -408,7 +285,7 @@ fn diff_considerig_ace(a: CardValue, b: CardValue) -> u8 {
         .unwrap_or(a as u8 - 1)
 }
 
-/// Returns `HighCard` if no hand >= `player_hand` is found
+/// Returns `HighCard` if no `Hand` >= `player_hand` is found
 #[must_use]
 pub fn highest_possible_hand(mut input_cards: Vec<Card>, player_hand: Option<Hand>) -> Hand {
     assert!(input_cards.len() == 7);
