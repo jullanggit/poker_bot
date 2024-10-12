c486fe37f8a24ffdb2722bf95c9938f8d0c43b77
jullang <jullanggit@proton.me>
2024-10-12 14:41:19 +0200

    fixes

--- benches/poker_bot_benchmark.rs
+++ benches/poker_bot_benchmark.rs
@@ -1,6 +1,5 @@
-use array_macro::array;
 use criterion::{criterion_group, criterion_main, Criterion};
-use poker_bot::{calculate, highest_possible_hand, Card, Hand, SIMD_LANES};
+use poker_bot::{calculate, highest_possible_hand, Card, Hand};
 use seq_macro::seq;
 
 fn bench_calculate(c: &mut Criterion) {
@@ -12,13 +11,13 @@ fn bench_highest_possible_hand(c: &mut Criterion) {
     c.bench_function("highest_possible_hand", |b| {
         b.iter(|| {
             seq!(_ in 0..7 {
-                let mut input_cardss = array![_ => vec!(
+                let input_cardss = vec!(
                     #(
                         Card::random(),
                     )*
-                ); SIMD_LANES];
+                );
             });
-            highest_possible_hand(&mut input_cardss, Some(Hand::random()))
+            highest_possible_hand(input_cardss, Some(Hand::random()))
         });
     });
 }
